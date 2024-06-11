fa41e18712e75db5d763f87c2bb17c7445ae0a14
heyyakash <akashsharma2002@gmail.com>
2024-06-11 22:13:36 +0530

    made log more better

--- pkg/CalculateResults.go
+++ pkg/CalculateResults.go
@@ -8,21 +8,34 @@ import (
 	"github.com/heyyakash/hammer/models"
 )
 
+var (
+	minTime time.Duration
+	maxTime time.Duration
+)
+
 func CalculateResults(responseTime <-chan time.Duration, statistics *models.Stats) {
 	var times []time.Duration
 	for t := range responseTime {
 		times = append(times, t)
 	}
-	fmt.Printf("\n\nHammer Results\n")
-	fmt.Printf("Average time\t2xx Response\t4xx Response\t5xx Response\tError Count\tTotal Requests\n")
-	fmt.Printf("%v\t\t%v\t\t%v\t\t%v\t\t%v\t\t%v\n\n",
-		Average(times),
-		atomic.LoadInt64(&statistics.Counter2xx),
-		atomic.LoadInt64(&statistics.Counter4xx),
-		atomic.LoadInt64(&statistics.Counter5xx),
-		atomic.LoadInt64(&statistics.ErrorCount),
-		atomic.LoadInt64(&statistics.Requests),
-	)
+	fmt.Printf("\nHammer Results\n\n")
+	fmt.Println("Latency Metrics:")
+	fmt.Printf("  Average Latency:   %v\n", Average(times))
+	fmt.Printf("  Minimum Latency:   %v\n", minTime)
+	fmt.Printf("  Maximum Latency:   %v\n", maxTime)
+
+	fmt.Println("\nResponse Metrics:")
+	fmt.Printf("  2xx Response:      %v\n", atomic.LoadInt64(&statistics.Counter2xx))
+	fmt.Printf("  4xx Response:      %v\n", atomic.LoadInt64(&statistics.Counter4xx))
+	fmt.Printf("  5xx Response:      %v\n", atomic.LoadInt64(&statistics.Counter5xx))
+
+	fmt.Println("\nError Metrics:")
+	fmt.Printf("  Error Count:       %v\n", atomic.LoadInt64(&statistics.ErrorCount))
+
+	fmt.Println("\nTotal Metrics:")
+	fmt.Printf("  Total Requests:    %v\n", atomic.LoadInt64(&statistics.Requests))
+	fmt.Println()
+
 }
 
 func Average(times []time.Duration) time.Duration {
@@ -32,6 +45,12 @@ func Average(times []time.Duration) time.Duration {
 	}
 	for _, i := range times {
 		sum += i
+		if i < minTime {
+			minTime = i
+		}
+		if i > maxTime {
+			maxTime = i
+		}
 	}
 	avg := sum / time.Duration((len(times)))
 	return avg
